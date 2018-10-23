6e2b0edacdb506b82f50cc8dcdbfc7e45365361f
Tony <tony_possamai@hotmail.com>
2018-10-23 16:27:06 +0100

    Adding style, API data into Js, organising partials

--- gulpfile.js
+++ gulpfile.js
@@ -1,79 +1,151 @@
-// Require Gulp first!
-const gulp = require('gulp'),
-
-  prettyError = require('gulp-prettyerror'),
-  sass = require('gulp-sass'),
-  autoprefixer = require('gulp-autoprefixer'),
-  rename = require('gulp-rename'),
-  cssnano = require('gulp-cssnano'),
-  uglify = require('gulp-uglify'),
-  eslint = require('gulp-eslint');
+// const gulp = require('gulp'),
+//   prettyError = require('gulp-prettyerror'),
+//   sass = require('gulp-sass'),
+//   autoprefixer = require('gulp-autoprefixer'),
+//   rename = require('gulp-rename'),
+//   cssnano = require('gulp-cssnano'),
+//   uglify = require('gulp-uglify-es').default,
+//   eslint = require('gulp-eslint');
   
-  var browserSync = require('browser-sync').create();
+//   var browserSync = require('browser-sync').create();
 
-// Create basic Gulp tasks
+// // Create basic Gulp tasks
 
-gulp.task('sass', function(done) {
-  gulp
-    .src('./sass/style.scss', { sourcemaps: true })
-    .pipe(prettyError())
-    .pipe(sass())
-    .pipe(
-      autoprefixer({
-        browsers: ['last 2 versions']
-      })
-    )
-    .pipe(gulp.dest('./build/css'))
-    .pipe(cssnano())
-    .pipe(rename('style.min.css'))
-    .pipe(gulp.dest('./build/css'));
+// gulp.task('sass', function(done) {
+//   gulp
+//     .src('./sass/style.scss', { sourcemaps: true })
+//     .pipe(prettyError())
+//     .pipe(sass())
+//     .pipe(
+//       autoprefixer({
+//         browsers: ['last 2 versions']
+//       })
+//     )
+//     .pipe(gulp.dest('./build/css'))
+//     .pipe(cssnano())
+//     .pipe(rename('style.min.css'))
+//     .pipe(gulp.dest('./build/css'));
 
   
-  done();
+//   done();
+// });
+
+// gulp.task('lint', function() {
+//   return (gulp
+//       .src(['./js/*.js'])
+//       // Also need to use it here...
+//       .pipe(prettyError())
+//       .pipe(eslint())
+//       .pipe(eslint.format())
+//       .pipe(eslint.failAfterError()) );
+// });
+
+// gulp.task(
+//   'script',
+//   gulp.series('lint', function() {
+//     return gulp
+//       .src('./js/*.js')
+//       .pipe(uglify())
+//       .pipe(
+//         rename({
+//           extname: '.min.js'
+//         })
+//       )
+//       .pipe(gulp.dest('./build/js'));
+//   })
+// );
+
+// // Set-up BrowserSync and watch
+
+// gulp.task('browser-sync', function() {
+//   browserSync.init({
+//     server: {
+//       baseDir: './build'
+//     }
+//   });
+
+//   gulp
+//     .watch(['build/css/*.css', 'build/js/*.js'])
+//     .on('change', browserSync.reload);
+// });
+
+// gulp.task('watch', function() {
+//   gulp.watch('js/*.js', gulp.series('script'));
+//   gulp.watch('sass/*.scss', gulp.series('sass'));
+// });
+
+// gulp.task('default', gulp.parallel('browser-sync', 'watch'));
+
+const gulp = require("gulp"),
+   rename = require("gulp-rename"),
+   uglify = require("gulp-uglify-es").default,
+   browserSync = require("browser-sync").create(),
+   eslint = require("gulp-eslint"),
+   prettyError = require("gulp-prettyerror"),
+   sass = require("gulp-sass"),
+   autoprefixer = require("gulp-autoprefixer"),
+   cssnano = require("gulp-cssnano");
+
+
+gulp.task("sass", function(done) {
+   gulp
+       .src("./sass/style.scss", { sourcemaps: true })
+       .pipe(prettyError())
+       .pipe(sass())
+       .pipe(
+           autoprefixer({
+               browsers: ["last 2 versions"]
+           })
+       )
+       .pipe(gulp.dest("./build/css"))
+       .pipe(cssnano())
+       .pipe(rename("style.min.css"))
+       .pipe(gulp.dest("./build/css"))
+       .on("end", done);
 });
 
-gulp.task('lint', function() {
-  return (gulp
-      .src(['./js/*.js'])
-      // Also need to use it here...
-      .pipe(prettyError())
-      .pipe(eslint())
-      .pipe(eslint.format())
-      .pipe(eslint.failAfterError()) );
+gulp.task("eslint", function(done) {
+   return gulp
+       .src(["./js/*.js"])
+       // Also need to use it here...
+       .pipe(prettyError())
+       .pipe(eslint())
+       .pipe(eslint.format())
+       .pipe(eslint.failAfterError())
+       .on("end", done);
 });
 
-gulp.task(
-  'scripts',
-  gulp.series('lint', function() {
-    return gulp
-      .src('./js/*.js')
-      .pipe(uglify())
-      .pipe(
-        rename({
-          extname: '.min.js'
-        })
-      )
-      .pipe(gulp.dest('./build/js'));
-  })
-);
+gulp.task("scripts", gulp.series("eslint", function() {
+   return gulp
+       .src("./js/*.js")
+       .pipe(uglify())
+       .pipe(
+           rename({
+               extname: ".min.js"
+           })
+       )
+       .pipe(gulp.dest("./build/js"));
+}));
 
 // Set-up BrowserSync and watch
 
-gulp.task('browser-sync', function() {
-  browserSync.init({
-    server: {
-      baseDir: './'
-    }
-  });
+gulp.task("browser-sync", function() {
+   browserSync.init({
+       server: {
+           baseDir: "./"
+       }
+   });
 
-  gulp
-    .watch(['build/css/*.css', 'build/js/*.js'])
-    .on('change', browserSync.reload);
+   gulp
+       .watch(["build/css/*.css", "build/js/*.js"])
+       .on("change", browserSync.reload);
 });
 
-gulp.task('watch', function() {
-  gulp.watch('js/*.js', gulp.series('scripts'));
-  gulp.watch('sass/*.scss', gulp.series('sass'));
+gulp.task("watch", function() {
+   gulp.watch("js/*.js", gulp.series("scripts"));
+   gulp.watch("sass/**/*.scss", gulp.series("sass"));
 });
 
-gulp.task('default', gulp.parallel('browser-sync', 'watch'));
+
+gulp.task("build", gulp.parallel("scripts", "sass"));
+gulp.task("default", gulp.series("build", gulp.parallel("browser-sync", "watch")));
--- js/script.js
+++ js/script.js
@@ -1,2 +1,32 @@
-var hello="hello!";
-console.log(hello);
+  $('select').on('change', function(){
+    var url = "https://api.nytimes.com/svc/topstories/v2/"+$('select').val()+".json";
+    url += '?' + $.param({
+    'api-key': "e3bf7fa9f4c84ef19df635f8634973d4"
+    });
+
+$('.header').toggleClass('.moving');    
+
+    $.ajax({
+        url: url,
+        method: 'GET',
+    }).done(function(data) {
+        $('.container').empty();
+
+        for (var i=0; i < 12; i++) {
+        let title = data.results[i].abstract;
+        let articleUrl = data.results[i].url;
+        let imageUrl = data.results[i].multimedia[4].url;  
+  
+        $("#title").append(data.results[i].abstract);
+        $('.container').append('<section class="articleContainer"><a href="'+ articleUrl +'" class="link"><h1 class="articleTitle">' + title + '</h1></a></section>');
+        $('.articleContainer').last().css("background-image", 'url('+ imageUrl +')');
+    }
+  
+  }).fail(function(err) {
+    throw err;
+  });
+
+
+})
+
+
